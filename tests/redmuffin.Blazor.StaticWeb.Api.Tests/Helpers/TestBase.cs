b50d40e89b4eacce61d0cee2c3d3e4274e06da79
Michael A. Volz <mvolz@redmuffin.de>
2025-06-18 21:27:07 +0200

    Refactor TestBase class for improved configuration setup
    Updated the `TestBase` class in `TestBase.cs` to include a namespace declaration. Removed the constructor and initialized the `Configuration` property directly at its declaration using a `ConfigurationBuilder`, simplifying the code while preserving functionality.

--- tests/redmuffin.Blazor.StaticWeb.Api.Tests/Helpers/TestBase.cs
+++ tests/redmuffin.Blazor.StaticWeb.Api.Tests/Helpers/TestBase.cs
@@ -1,14 +1,11 @@
 using Microsoft.Extensions.Configuration;
 
+namespace redmuffin.Blazor.StaticWeb.Api.Tests.Helpers;
+
 public class TestBase
 {
-    protected IConfiguration Configuration { get; private set; }
-
-    public TestBase()
-    {
-        Configuration = new ConfigurationBuilder()
-            .SetBasePath(AppContext.BaseDirectory) // Points to bin/debug/net8.0
-            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-            .Build();
-    }
+	protected IConfiguration Configuration { get; private set; } = new ConfigurationBuilder()
+		.SetBasePath(AppContext.BaseDirectory) // Points to bin/debug/net8.0
+		.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+		.Build();
 }
