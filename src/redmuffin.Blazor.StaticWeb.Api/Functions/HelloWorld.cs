5ea57c347196d6c7de1146649d5e76522694ef5d
Michael A. Volz <mvolz@redmuffin.de>
2025-06-19 09:07:12 +0200

    Update .editorconfig and add HelloWorld function
    Modified diagnostic severity levels in .editorconfig, including setting category-design to warning. Added rules for enforcing underscores in namespaces and method names. Updated HelloWorld.cs with necessary using directives and implemented an Azure Functions HTTP trigger that logs a message and returns a welcome response.

---
+++ src/redmuffin.Blazor.StaticWeb.Api/Functions/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace redmuffin.Blazor.StaticWeb.Api.Functions;
+
+public class HelloWorld(ILogger<HelloWorld> logger)
+{
+	private static readonly Action<ILogger, string, Exception?> Log_TriggerProcessed =
+		LoggerMessage.Define<string>(LogLevel.Information, new EventId(1, "Hello"), "C# HTTP trigger function processed a request. {Class}!");
+
+	[Function("HelloWorld")]
+	public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+	{
+		Log_TriggerProcessed(logger, nameof(HelloWorld), null);
+		return new OkObjectResult("Welcome to Azure Functions!");
+	}
+}
