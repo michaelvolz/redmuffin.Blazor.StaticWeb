1c49abb3bf9f9a4afb9c790739ac774f2b7033a1
Michael A. Volz <mvolz@redmuffin.de>
2025-06-19 18:10:58 +0200

    Enable live reload for .NET applications
    Updated scripts to use `dotnet watch run` for automatic
    recompilation and restarting of applications in both
    `src/redmuffin.Blazor.StaticWeb.Api/` and
    `src/redmuffin.Blazor.StaticWeb/` directories.

--- Start.ps1
+++ Start.ps1
@@ -1,10 +1,10 @@
 Push-Location "src/redmuffin.Blazor.StaticWeb.Api/"
     Start-Process -NoNewWindow "C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\Extensions\Microsoft\Azure Storage Emulator\azurite.exe"
-    Start-Process -NoNewWindow powershell -ArgumentList "-Command dotnet run --port 7184"
+    Start-Process -NoNewWindow powershell -ArgumentList "-Command dotnet watch run --port 7184"
 Pop-Location
 
 Push-Location "src/redmuffin.Blazor.StaticWeb/"
-    Start-Process -NoNewWindow powershell -ArgumentList "-Command dotnet run"
+    Start-Process -NoNewWindow powershell -ArgumentList "-Command dotnet watch run"
 Pop-Location
 
 swa start http://localhost:5233 --api-location http://localhost:7184/api
