95c68f7f42248c512246d0e554cc7a717349cf77
Michael A. Volz <mvolz@redmuffin.de>
2025-06-19 10:56:22 +0200

    Remove RaindropJsonSerializerContext class and imports
    This commit removes the `RaindropJsonSerializerContext` class,
    the `using System.Text.Json.Serialization;` directive, the
    namespace declaration, and the `[JsonSerializable]` attribute
    from the codebase, streamlining the serialization context.

--- src/redmuffin.Blazor.StaticWeb/Common/Raindrop/RaindropJsonSerializerContext.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace redmuffin.Blazor.StaticWeb.Common.Raindrop;
-
-[JsonSerializable(typeof(List<RaindropItem>))]
-public partial class RaindropJsonSerializerContext : JsonSerializerContext
-{
-}
