842f90e93727072ba03250648010c1cb08fb61bf
Michael A. Volz <mvolz@redmuffin.de>
2025-06-19 12:29:36 +0200

    Extract CreatorReferenceConverter for JSON handling
    Extracted the CreatorReferenceConverter class in CreatorReferenceConverter.cs to manage custom serialization and deserialization of CreatorReference objects. Updated Highlight.cs to remove redundant using directives and reference the new converter for the CreatorRef property.

--- src/redmuffin.Blazor.StaticWeb.Common/Raindrop/Highlight.cs
+++ src/redmuffin.Blazor.StaticWeb.Common/Raindrop/Highlight.cs
@@ -1,4 +1,3 @@
-using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace redmuffin.Blazor.StaticWeb.Common.Raindrop;
@@ -20,34 +19,4 @@ public class Highlight
     [JsonPropertyName("creatorRef")]
     [JsonConverter(typeof(CreatorReferenceConverter))]
     public CreatorReference? CreatorRef { get; set; } = null;
-}
-
-public class CreatorReferenceConverter : JsonConverter<CreatorReference?>
-{
-    public override CreatorReference? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        if (reader.TokenType == JsonTokenType.Number)
-        {
-            var id = reader.GetInt64();
-            return new CreatorReference { Id = id };
-        }
-        else if (reader.TokenType == JsonTokenType.StartObject)
-        {
-            return JsonSerializer.Deserialize<CreatorReference>(ref reader, options);
-        }
-
-        return null;
-    }
-
-    public override void Write(Utf8JsonWriter writer, CreatorReference? value, JsonSerializerOptions options)
-    {
-        if (value != null)
-        {
-            JsonSerializer.Serialize(writer, value, options);
-        }
-        else
-        {
-            writer.WriteNullValue();
-        }
-    }
 }
