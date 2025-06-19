6916274a300e3887f9e58052dc2be617e4456f0c
Michael A. Volz <mvolz@redmuffin.de>
2025-06-19 12:29:05 +0200

    Improve formatting and property initialization in RaindropItem
    This commit enhances the readability of `RaindropItem.cs` by adjusting the indentation of property declarations. It also updates the initialization of some properties to use shorthand syntax (e.g., `new()`), while maintaining the existing JSON property attributes for serialization.

--- src/redmuffin.Blazor.StaticWeb.Common/Raindrop/RaindropItem.cs
+++ src/redmuffin.Blazor.StaticWeb.Common/Raindrop/RaindropItem.cs
@@ -1,5 +1,5 @@
-using JetBrains.Annotations;
 using System.Text.Json.Serialization;
+using JetBrains.Annotations;
 
 #pragma warning disable MA0048 //Disable warning for file name not matching type name
 
@@ -8,60 +8,60 @@ namespace redmuffin.Blazor.StaticWeb.Common.Raindrop;
 [UsedImplicitly]
 public class RaindropItem
 {
-    [JsonPropertyName("_id")]
-    public long Id { get; set; }
+	[JsonPropertyName("_id")]
+	public long Id { get; set; }
 
-    [JsonPropertyName("link")]
-    public string Link { get; set; } = string.Empty;
+	[JsonPropertyName("link")]
+	public string Link { get; set; } = string.Empty;
 
-    [JsonPropertyName("title")]
-    public string Title { get; set; } = string.Empty;
+	[JsonPropertyName("title")]
+	public string Title { get; set; } = string.Empty;
 
-    [JsonPropertyName("excerpt")]
-    public string Excerpt { get; set; } = string.Empty;
+	[JsonPropertyName("excerpt")]
+	public string Excerpt { get; set; } = string.Empty;
 
-    [JsonPropertyName("note")]
-    public string Note { get; set; } = string.Empty;
+	[JsonPropertyName("note")]
+	public string Note { get; set; } = string.Empty;
 
-    [JsonPropertyName("type")]
-    public string Type { get; set; } = string.Empty;
+	[JsonPropertyName("type")]
+	public string Type { get; set; } = string.Empty;
 
-    [JsonPropertyName("user")]
-    public UserReference User { get; set; } = new UserReference();
+	[JsonPropertyName("user")]
+	public UserReference User { get; set; } = new();
 
-    [JsonPropertyName("cover")]
-    public string Cover { get; set; } = string.Empty;
+	[JsonPropertyName("cover")]
+	public string Cover { get; set; } = string.Empty;
 
-    [JsonPropertyName("media")]
-    public IList<MediaItem> Media { get; set; } = new List<MediaItem>();
+	[JsonPropertyName("media")]
+	public IList<MediaItem> Media { get; set; } = new List<MediaItem>();
 
-    [JsonPropertyName("tags")]
-    public IList<string> Tags { get; set; } = new List<string>();
+	[JsonPropertyName("tags")]
+	public IList<string> Tags { get; set; } = new List<string>();
 
-    [JsonPropertyName("important")]
-    public bool Important { get; set; }
+	[JsonPropertyName("important")]
+	public bool Important { get; set; }
 
-    [JsonPropertyName("reminder")]
-    public Reminder Reminder { get; set; } = new Reminder();
+	[JsonPropertyName("reminder")]
+	public Reminder Reminder { get; set; } = new();
 
-    [JsonPropertyName("removed")]
-    public bool Removed { get; set; }
+	[JsonPropertyName("removed")]
+	public bool Removed { get; set; }
 
-    [JsonPropertyName("created")]
-    public DateTime Created { get; set; } = DateTime.MinValue;
+	[JsonPropertyName("created")]
+	public DateTime Created { get; set; } = DateTime.MinValue;
 
-    [JsonPropertyName("collection")]
-    public CollectionReference Collection { get; set; } = new CollectionReference();
+	[JsonPropertyName("collection")]
+	public CollectionReference Collection { get; set; } = new();
 
-    [JsonPropertyName("highlights")]
-    public IList<Highlight> Highlights { get; set; } = new List<Highlight>();
+	[JsonPropertyName("highlights")]
+	public IList<Highlight> Highlights { get; set; } = new List<Highlight>();
 
-    [JsonPropertyName("domain")]
-    public string Domain { get; set; } = string.Empty;
+	[JsonPropertyName("domain")]
+	public string Domain { get; set; } = string.Empty;
 
-    [JsonPropertyName("collectionId")]
-    public long CollectionId { get; set; }
+	[JsonPropertyName("collectionId")]
+	public long CollectionId { get; set; }
 
-    [JsonPropertyName("sort")]
-    public long Sort { get; set; }
+	[JsonPropertyName("sort")]
+	public long Sort { get; set; }
 }
