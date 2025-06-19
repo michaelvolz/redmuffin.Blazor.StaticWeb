0142b4f3c3f33e730b6a33d1027bfd09066cdc64
Michael A. Volz <mvolz@redmuffin.de>
2025-06-19 10:53:56 +0200

    Refactor HelloWorld class and update logging mechanism
    - Changed `HelloWorld` to a partial class for better organization.
    - Replaced static readonly logging field with a structured partial method using `LoggerMessage`.
    - Updated the `Run` method to call the new logging method without the `null` argument.

--- src/redmuffin.Blazor.StaticWeb.Api/Functions/HelloWorld.cs
+++ src/redmuffin.Blazor.StaticWeb.Api/Functions/HelloWorld.cs
@@ -5,15 +5,15 @@ using Microsoft.Extensions.Logging;
 
 namespace redmuffin.Blazor.StaticWeb.Api.Functions;
 
-public class HelloWorld(ILogger<HelloWorld> logger)
+public partial class HelloWorld(ILogger<HelloWorld> logger)
 {
-	private static readonly Action<ILogger, string, Exception?> Log_TriggerProcessed =
-		LoggerMessage.Define<string>(LogLevel.Information, new EventId(1, "Hello"), "C# HTTP trigger function processed a request. {Class}!");
+	[LoggerMessage(1, LogLevel.Information, "C# HTTP trigger function processed a request. {Class}!", EventName = "Log_TriggerProcessed")]
+	public static partial void Log_TriggerProcessed(ILogger logger, string @class);
 
 	[Function("HelloWorld")]
 	public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
 	{
-		Log_TriggerProcessed(logger, nameof(HelloWorld), null);
+		Log_TriggerProcessed(logger, nameof(HelloWorld));
 		return new OkObjectResult("Welcome to Azure Functions!");
 	}
 }
