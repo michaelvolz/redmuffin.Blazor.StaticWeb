036d43c063fc0b2889ebd71f39f366501f011716
Michael A. Volz <mvolz@redmuffin.de>
2025-07-09 21:39:20 +0200

    feat(di): register PerformanceMetricsService in dependency injection
    Adds service registration for IPerformanceMetricsService implementation.
    Enables proper dependency injection for PageLoadSpeed component
    and supports scalable service architecture.

--- src/redmuffin.Blazor.StaticWeb/Program.cs
+++ src/redmuffin.Blazor.StaticWeb/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using redmuffin.Blazor.StaticWeb;
 using redmuffin.Blazor.StaticWeb.Core.Services;
+using redmuffin.Blazor.StaticWeb.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IWarmupService, WarmupService>();
+builder.Services.AddScoped<IPerformanceMetricsService, PerformanceMetricsService>();
 
 await builder.Build().RunAsync().ConfigureAwait(false);
