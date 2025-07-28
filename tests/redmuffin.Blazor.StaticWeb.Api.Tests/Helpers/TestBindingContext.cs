c2162e1b77bc1fc801378496e5c632936ef7f880
Michael A. Volz <mvolz@redmuffin.de>
2025-07-28 21:27:04 +0200

    refactor(test): remove legacy test helper classes
    These helper classes will be replaced with new standardized test helpers that follow modern TUnit patterns and best practices.

--- tests/redmuffin.Blazor.StaticWeb.Api.Tests/Helpers/TestBindingContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Azure.Functions.Worker;
-
-namespace redmuffin.Blazor.StaticWeb.Api.Tests.Helpers;
-
-public class TestBindingContext : BindingContext
-{
-    public override IReadOnlyDictionary<string, object?> BindingData =>
-        new Dictionary<string, object?>(StringComparer.OrdinalIgnoreCase);
-}
--- tests/redmuffin.Blazor.StaticWeb.Api.Tests/Helpers/TestTraceContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Azure.Functions.Worker;
-
-namespace redmuffin.Blazor.StaticWeb.Api.Tests.Helpers;
-
-public class TestTraceContext : TraceContext
-{
-    public override string TraceParent => null!;
-    public override string TraceState => null!;
-}
