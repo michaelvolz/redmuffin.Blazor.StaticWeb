fe0a46c57c299be558dc929843086d4781d04add
Michael A. Volz <mvolz@redmuffin.de>
2026-05-02 18:41:43 +0200

    chore(config): replace commitlint preset with explicit rules
    Replace @commitlint/config-conventional extend with explicitly
    defined rules so the configuration is self-contained and
    version-independent.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,30 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'body-leading-blank': [2, 'always'],
-    'body-empty': [2, 'never'],
-    'body-max-line-length': [2, 'always', 100],
-    'footer-leading-blank': [2, 'always'],
-  },
+module.exports = {
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "style",
+                "refactor",
+                "perf",
+                "test",
+                "build",
+                "ci",
+                "chore",
+                "revert",
+            ],
+        ],
+        "type-case": [2, "always", "lower-case"],
+        "type-empty": [2, "never"],
+        "subject-empty": [2, "never"],
+        "subject-full-stop": [2, "never", "."],
+        "header-max-length": [2, "always", 100],
+        "body-empty": [2, "never"],
+        "body-leading-blank": [2, "always"],
+        "body-max-line-length": [2, "always", 100],
+        "footer-leading-blank": [2, "always"],
+    },
 };
